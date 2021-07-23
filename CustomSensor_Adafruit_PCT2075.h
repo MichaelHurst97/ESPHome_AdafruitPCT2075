9b9ee5c300d1d3d0486a616e5aaef1cbb95b55e8
MichaelHurst97 <44029068+MichaelHurst97@users.noreply.github.com>
2021-07-23 22:09:56 +0200

    Update CustomSensor_Adafruit_PCT2075.h

--- CustomSensor_Adafruit_PCT2075.h
+++ CustomSensor_Adafruit_PCT2075.h
@@ -6,18 +6,18 @@ class CustomPCT2075Temp: public PollingComponent, public Sensor {
 public:
     Adafruit_PCT2075 PCT2075;
 	
-    CustomPCT2075Temp(): PollingComponent(10000) {} 
+    CustomPCT2075Temp(): PollingComponent(10000) {} // refresh update() every 10sec
 
     float get_setup_priority() const override {
-        return esphome::setup_priority::BUS;
+        return esphome::setup_priority::BUS; // initialize as I2C / bus device
     }
 
     void setup() override {
-        PCT2075.begin();
+        PCT2075.begin(); // initialize sensor
     }
 	
     void update() override {
-        float temperature = PCT2075.getTemperature();
-        publish_state(temperature); 
+        float temperature = PCT2075.getTemperature(); // grab temperature readings
+        publish_state(temperature); // pass the reading on to ESPHome & Home Assistant
     }
 };
